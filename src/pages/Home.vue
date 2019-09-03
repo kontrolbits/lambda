5910f1aa78d8338192fdcbefc1f91f364e72ea93
G-campos <gabodecampos@gmail.com>
2019-09-03 11:35:56 -0300

    add home

---
+++ src/pages/Home.vue
@@ -0,0 +1,89 @@
+<template>
+<q-page class="row">
+    <div class="col-12 q-pa-md q-gutter-sm">
+        <q-breadcrumbs style="font-size: 15px">
+            <q-breadcrumbs-el label="Index" />
+            <q-breadcrumbs-el label="Home" />
+        </q-breadcrumbs>
+    </div>
+    <div class="col-12 row">
+        <div class="col-3 row">
+            <div class="col-12">
+                <q-card flat bordered>
+                    <q-car-section>
+                        <div class="row items-center no-wrap">
+                            <div class="col">
+                                <div class="text-h6">Our Planet</div>
+                                <div class="text-subtitle2">by John Doe</div>
+                            </div>
+                        </div>
+                    </q-car-section>
+                </q-card>
+            </div>
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+        </div>
+        <div class="col-3">
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+        </div>
+        <div class="col-3">
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+        </div>
+        <div class="col-3">
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+        </div>
+    </div>
+    <div class="col-12 row">
+        <div class="col-12 row">
+            <div class="col-9"></div>
+            <div class="col-2"></div>
+            <div class="col-1"></div>
+        </div>
+        <div class="col-12"></div>
+        <div class="col-12 row flex flex-center">
+            <div class="col-2"></div>
+            <div class="col-2"></div>
+            <div class="col-2"></div>
+            <div class="col-2"></div>
+            <div class="col-2"></div>
+        </div>
+    </div>
+    <div class="col-12 row">
+        <div class="col-3 row">
+            <div class="col-12"></div>
+            <div class="col-6"></div>
+            <div class="col-6"></div>
+        </div>
+        <div class="col-3">
+            <div class="col-12"></div>
+            <div class="col-6"></div>
+            <div class="col-6"></div>
+        </div>
+        <div class="col-3">
+            <div class="col-12"></div>
+            <div class="col-6"></div>
+            <div class="col-6"></div>
+        </div>
+        <div class="col-3">
+            <div class="col-12"></div>
+            <div class="col-6"></div>
+            <div class="col-6"></div>
+        </div>
+    </div>
+</q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
