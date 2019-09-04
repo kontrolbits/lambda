b85b9ffac2935107707fcd0359a89a9cb3a4904e
G-campos <gabodecampos@gmail.com>
2019-09-04 08:33:09 -0300

    up home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -16,8 +16,35 @@
                                 <div class="text-h6">Our Planet</div>
                                 <div class="text-subtitle2">by John Doe</div>
                             </div>
+                            <div class="col-auto">
+                                <q-btn color="grey-7" round flat icon="more_vert">
+                                    <q-menu cover auto-close>
+                                        <q-list>
+                                            <q-item clickable>
+                                                <q-item-section>Remove Card</q-item-section>
+                                            </q-item>
+                                            <q-item clickable>
+                                                <q-item-section>Send Feedback</q-item-section>
+                                            </q-item>
+                                            <q-item clickable>
+                                                <q-item-section>Share</q-item-section>
+                                            </q-item>
+                                        </q-list>
+                                    </q-menu>
+                                </q-btn>
+                            </div>
                         </div>
                     </q-car-section>
+                    <q-card-section>
+                        {{ lorem }}
+                    </q-card-section>
+
+                    <q-separator />
+
+                    <q-card-actions>
+                        <q-btn flat>Action 1</q-btn>
+                        <q-btn flat>Action 2</q-btn>
+                    </q-card-actions>
                 </q-card>
             </div>
             <div class="col-12"></div>
@@ -84,6 +111,11 @@
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  data () {
+    return {
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+  }
 }
 </script>
