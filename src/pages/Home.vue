a8bfde7591a2b54684a90c9f256aeb61020829aa
G-campos <gabodecampos@gmail.com>
2019-09-06 10:33:30 -0300

    up semi-final home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -219,30 +219,13 @@
     </div>
     <div class="col-12 q-ml-xs q-mr-md q-col-gutter-md row">
         <div class="col-md-3 col-sm-12 col-xs-12">
-            <!-- card1 -->
+            <!-- card6 -->
             <q-card bordered>
                 <q-card-section>
                     <div class="row items-center no-wrap">
                         <div class="col q-pl-sm">
                             <div class="text-h6">Our Planet</div>
                             <div class="text-subtitle2">by John Doe</div>
-                        </div>
-                        <div class="col-auto">
-                            <q-btn color="grey-7" round flat icon="more_vert">
-                                <q-menu cover auto-close>
-                                    <q-list>
-                                        <q-item clickable>
-                                            <q-item-section>Remove Card</q-item-section>
-                                        </q-item>
-                                        <q-item clickable>
-                                            <q-item-section>Send Feedback</q-item-section>
-                                        </q-item>
-                                        <q-item clickable>
-                                            <q-item-section>Share</q-item-section>
-                                        </q-item>
-                                    </q-list>
-                                </q-menu>
-                            </q-btn>
                         </div>
                     </div>
                 </q-card-section>
@@ -252,37 +235,20 @@
 
                 <q-separator />
 
-                <q-card-actions>
-                    <q-btn flat>Action 1</q-btn>
-                    <q-btn flat>Action 2</q-btn>
+                <q-card-actions class="row flex flex-center">
+                    <q-btn class="col-5" flat>Action 1</q-btn>
+                    <q-btn class="col-5" flat>Action 2</q-btn>
                 </q-card-actions>
             </q-card>
         </div>
         <div class="col-md-3 col-sm-12 col-xs-12">
-            <!-- card2 -->
+            <!-- card7 -->
             <q-card bordered>
                 <q-card-section>
                     <div class="row items-center no-wrap">
                         <div class="col q-pl-sm">
                             <div class="text-h6">Our Planet</div>
                             <div class="text-subtitle2">by John Doe</div>
-                        </div>
-                        <div class="col-auto">
-                            <q-btn color="grey-7" round flat icon="more_vert">
-                                <q-menu cover auto-close>
-                                    <q-list>
-                                        <q-item clickable>
-                                            <q-item-section>Remove Card</q-item-section>
-                                        </q-item>
-                                        <q-item clickable>
-                                            <q-item-section>Send Feedback</q-item-section>
-                                        </q-item>
-                                        <q-item clickable>
-                                            <q-item-section>Share</q-item-section>
-                                        </q-item>
-                                    </q-list>
-                                </q-menu>
-                            </q-btn>
                         </div>
                     </div>
                 </q-card-section>
@@ -292,37 +258,20 @@
 
                 <q-separator />
 
-                <q-card-actions>
-                    <q-btn flat>Action 1</q-btn>
-                    <q-btn flat>Action 2</q-btn>
+                <q-card-actions class="row flex flex-center">
+                    <q-btn class="col-5" flat>Action 1</q-btn>
+                    <q-btn class="col-5" flat>Action 2</q-btn>
                 </q-card-actions>
             </q-card>
         </div>
         <div class="col-md-3 col-sm-12 col-xs-12">
-            <!-- card3 -->
+            <!-- card8 -->
             <q-card bordered>
                 <q-card-section>
                     <div class="row items-center no-wrap">
                         <div class="col q-pl-sm">
                             <div class="text-h6">Our Planet</div>
                             <div class="text-subtitle2">by John Doe</div>
-                        </div>
-                        <div class="col-auto">
-                            <q-btn color="grey-7" round flat icon="more_vert">
-                                <q-menu cover auto-close>
-                                    <q-list>
-                                        <q-item clickable>
-                                            <q-item-section>Remove Card</q-item-section>
-                                        </q-item>
-                                        <q-item clickable>
-                                            <q-item-section>Send Feedback</q-item-section>
-                                        </q-item>
-                                        <q-item clickable>
-                                            <q-item-section>Share</q-item-section>
-                                        </q-item>
-                                    </q-list>
-                                </q-menu>
-                            </q-btn>
                         </div>
                     </div>
                 </q-card-section>
@@ -332,37 +281,20 @@
 
                 <q-separator />
 
-                <q-card-actions>
-                    <q-btn flat>Action 1</q-btn>
-                    <q-btn flat>Action 2</q-btn>
+                <q-card-actions class="row flex flex-center">
+                    <q-btn class="col-5" flat>Action 1</q-btn>
+                    <q-btn class="col-5" flat>Action 2</q-btn>
                 </q-card-actions>
             </q-card>
         </div>
-        <div class="col-md-3 col-sm-12 col-xs-12">
-            <!-- card4 -->
+        <div class="col-md-3 col-sm-12 col-xs-12 q-mb-sm">
+            <!-- card9 -->
             <q-card bordered>
                 <q-card-section>
                     <div class="row items-center no-wrap">
                         <div class="col q-pl-sm">
                             <div class="text-h6">Our Planet</div>
                             <div class="text-subtitle2">by John Doe</div>
-                        </div>
-                        <div class="col-auto">
-                            <q-btn color="grey-7" round flat icon="more_vert">
-                                <q-menu cover auto-close>
-                                    <q-list>
-                                        <q-item clickable>
-                                            <q-item-section>Remove Card</q-item-section>
-                                        </q-item>
-                                        <q-item clickable>
-                                            <q-item-section>Send Feedback</q-item-section>
-                                        </q-item>
-                                        <q-item clickable>
-                                            <q-item-section>Share</q-item-section>
-                                        </q-item>
-                                    </q-list>
-                                </q-menu>
-                            </q-btn>
                         </div>
                     </div>
                 </q-card-section>
@@ -372,9 +304,9 @@
 
                 <q-separator />
 
-                <q-card-actions>
-                    <q-btn flat>Action 1</q-btn>
-                    <q-btn flat>Action 2</q-btn>
+                <q-card-actions class="row flex flex-center">
+                    <q-btn class="col-5" flat>Action 1</q-btn>
+                    <q-btn class="col-5" flat>Action 2</q-btn>
                 </q-card-actions>
             </q-card>
         </div>
