ee9705557f56e7d9f6d818834f01d92b2cfde7b1
G-campos <gabodecampos@gmail.com>
2019-09-06 09:27:27 -0300

    up home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -172,18 +172,18 @@
         <!-- card5 -->
         <q-card bordered>
             <q-card-section>
-                <div class="row items-center no-wrap">
-                    <div class="col-10 q-pl-sm">
+                <div class="row items-center no-wrap ">
+                    <div class="col q-pl-sm ">
                         <div class="text-h6">Our Planet</div>
                         <!-- <div class="text-subtitle2">by John Doe</div> -->
                     </div>
-                    <div class="col-2 q-pl-sm">
+                    <q-space />
+                    <div class="col-auto q-pl-sm">
                         <q-btn-toggle class="desktop-only" color="primary" v-model="dataGrafico" toggle-color="secondary" :options="[
                                 {label: 'Dia', value: 'dia'},
                                 {label: 'Mês', value: 'mes'},
                                 {label: 'Ano', value: 'ano'}
-                            ]"
-                        />
+                            ]" />
                         <q-btn class="mobile-only" color="grey-7" round flat icon="more_vert">
                             <q-menu cover auto-close>
                                 <q-list>
@@ -203,7 +203,7 @@
                 </div>
             </q-card-section>
             <q-card-section>
-                {{ lorem }}
+                <img src="../statics/exemplo-grafico.jpg" style="width:100%; height:300px;">
             </q-card-section>
 
             <q-separator />
@@ -217,26 +217,166 @@
             </q-card-actions>
         </q-card>
     </div>
-    <div class="col-12 row">
-        <div class="col-3 row">
-            <div class="col-12"></div>
-            <div class="col-6"></div>
-            <div class="col-6"></div>
+    <div class="col-12 q-ml-xs q-mr-md q-col-gutter-md row">
+        <div class="col-md-3 col-sm-12 col-xs-12">
+            <!-- card1 -->
+            <q-card bordered>
+                <q-card-section>
+                    <div class="row items-center no-wrap">
+                        <div class="col q-pl-sm">
+                            <div class="text-h6">Our Planet</div>
+                            <div class="text-subtitle2">by John Doe</div>
+                        </div>
+                        <div class="col-auto">
+                            <q-btn color="grey-7" round flat icon="more_vert">
+                                <q-menu cover auto-close>
+                                    <q-list>
+                                        <q-item clickable>
+                                            <q-item-section>Remove Card</q-item-section>
+                                        </q-item>
+                                        <q-item clickable>
+                                            <q-item-section>Send Feedback</q-item-section>
+                                        </q-item>
+                                        <q-item clickable>
+                                            <q-item-section>Share</q-item-section>
+                                        </q-item>
+                                    </q-list>
+                                </q-menu>
+                            </q-btn>
+                        </div>
+                    </div>
+                </q-card-section>
+                <q-card-section>
+                    {{ lorem }}
+                </q-card-section>
+
+                <q-separator />
+
+                <q-card-actions>
+                    <q-btn flat>Action 1</q-btn>
+                    <q-btn flat>Action 2</q-btn>
+                </q-card-actions>
+            </q-card>
         </div>
-        <div class="col-3">
-            <div class="col-12"></div>
-            <div class="col-6"></div>
-            <div class="col-6"></div>
+        <div class="col-md-3 col-sm-12 col-xs-12">
+            <!-- card2 -->
+            <q-card bordered>
+                <q-card-section>
+                    <div class="row items-center no-wrap">
+                        <div class="col q-pl-sm">
+                            <div class="text-h6">Our Planet</div>
+                            <div class="text-subtitle2">by John Doe</div>
+                        </div>
+                        <div class="col-auto">
+                            <q-btn color="grey-7" round flat icon="more_vert">
+                                <q-menu cover auto-close>
+                                    <q-list>
+                                        <q-item clickable>
+                                            <q-item-section>Remove Card</q-item-section>
+                                        </q-item>
+                                        <q-item clickable>
+                                            <q-item-section>Send Feedback</q-item-section>
+                                        </q-item>
+                                        <q-item clickable>
+                                            <q-item-section>Share</q-item-section>
+                                        </q-item>
+                                    </q-list>
+                                </q-menu>
+                            </q-btn>
+                        </div>
+                    </div>
+                </q-card-section>
+                <q-card-section>
+                    {{ lorem }}
+                </q-card-section>
+
+                <q-separator />
+
+                <q-card-actions>
+                    <q-btn flat>Action 1</q-btn>
+                    <q-btn flat>Action 2</q-btn>
+                </q-card-actions>
+            </q-card>
         </div>
-        <div class="col-3">
-            <div class="col-12"></div>
-            <div class="col-6"></div>
-            <div class="col-6"></div>
+        <div class="col-md-3 col-sm-12 col-xs-12">
+            <!-- card3 -->
+            <q-card bordered>
+                <q-card-section>
+                    <div class="row items-center no-wrap">
+                        <div class="col q-pl-sm">
+                            <div class="text-h6">Our Planet</div>
+                            <div class="text-subtitle2">by John Doe</div>
+                        </div>
+                        <div class="col-auto">
+                            <q-btn color="grey-7" round flat icon="more_vert">
+                                <q-menu cover auto-close>
+                                    <q-list>
+                                        <q-item clickable>
+                                            <q-item-section>Remove Card</q-item-section>
+                                        </q-item>
+                                        <q-item clickable>
+                                            <q-item-section>Send Feedback</q-item-section>
+                                        </q-item>
+                                        <q-item clickable>
+                                            <q-item-section>Share</q-item-section>
+                                        </q-item>
+                                    </q-list>
+                                </q-menu>
+                            </q-btn>
+                        </div>
+                    </div>
+                </q-card-section>
+                <q-card-section>
+                    {{ lorem }}
+                </q-card-section>
+
+                <q-separator />
+
+                <q-card-actions>
+                    <q-btn flat>Action 1</q-btn>
+                    <q-btn flat>Action 2</q-btn>
+                </q-card-actions>
+            </q-card>
         </div>
-        <div class="col-3">
-            <div class="col-12"></div>
-            <div class="col-6"></div>
-            <div class="col-6"></div>
+        <div class="col-md-3 col-sm-12 col-xs-12">
+            <!-- card4 -->
+            <q-card bordered>
+                <q-card-section>
+                    <div class="row items-center no-wrap">
+                        <div class="col q-pl-sm">
+                            <div class="text-h6">Our Planet</div>
+                            <div class="text-subtitle2">by John Doe</div>
+                        </div>
+                        <div class="col-auto">
+                            <q-btn color="grey-7" round flat icon="more_vert">
+                                <q-menu cover auto-close>
+                                    <q-list>
+                                        <q-item clickable>
+                                            <q-item-section>Remove Card</q-item-section>
+                                        </q-item>
+                                        <q-item clickable>
+                                            <q-item-section>Send Feedback</q-item-section>
+                                        </q-item>
+                                        <q-item clickable>
+                                            <q-item-section>Share</q-item-section>
+                                        </q-item>
+                                    </q-list>
+                                </q-menu>
+                            </q-btn>
+                        </div>
+                    </div>
+                </q-card-section>
+                <q-card-section>
+                    {{ lorem }}
+                </q-card-section>
+
+                <q-separator />
+
+                <q-card-actions>
+                    <q-btn flat>Action 1</q-btn>
+                    <q-btn flat>Action 2</q-btn>
+                </q-card-actions>
+            </q-card>
         </div>
     </div>
 </q-page>
